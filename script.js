48489eb113202ac3b50044f4e00afbb1c3cc6051
Sultan-hazazi <hazazi1234stn@gmail.com>
2026-02-13 17:01:33 +0300

    Enhance switchLang function for Arabic support

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 let lang = "en";
 
-function switchLang(){
-if(lang=="en"){
-document.getElementById("aboutTitle").innerText="نبذة عني";
-document.getElementById("aboutText").innerText="طالب هندسة حاسب متخصص في الأنظمة المدمجة والمتحكمات الدقيقة باستخدام C و STM32";
-document.getElementById("skillsTitle").innerText="المهارات";
-document.getElementById("projectsTitle").innerText="المشاريع";
-lang="ar";
-}else{
+function switchLang() {
+const body = document.getElementById("body");
+
+if (lang === "en") {
+body.style.direction = "rtl";
+body.style.textAlign = "right";
+
+document.getElementById("aboutTitle").innerText = "نبذة عني";
+document.getElementById("aboutText").innerText =
+"طالب هندسة حاسب متخصص في الأنظمة المدمجة والمتحكمات الدقيقة باستخدام C و STM32";
+
+document.getElementById("skillsTitle").innerText = "المهارات";
+document.getElementById("projectsTitle").innerText = "المشاريع";
+
+document.getElementById("uniTitle").innerText = "مشاريع الجامعة";
+document.getElementById("elecTitle").innerText = "مشاريع كهرباء وإلكترونيات";
+document.getElementById("embTitle").innerText = "مشاريع الأنظمة المدمجة";
+
+lang = "ar";
+} 
+else {
 location.reload();
 }
 }
